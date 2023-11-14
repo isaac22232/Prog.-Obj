460a5aeb0b4acb67946ba1d3b89c7d0adab12f63
isaac22232 <tornadojaimepartida@gmail.com>
2023-11-13 20:04:20 -0600

    Actividad 23

---
+++ _p139_HolaMundoGUI.java
@@ -0,0 +1,24 @@
+import java.awt.Font;
+
+import javax.swing.*; 
+
+public class _p139_HolaMundoGUI extends JFrame {
+
+    private JLabel lblSaludo;
+
+    public _p139_HolaMundoGUI(){
+        setLayout(null);
+        lblSaludo = new JLabel("Hola Mundo GUI, Bienvenido");
+        lblSaludo.setFont(new Font("Arial", Font.BOLD,20));
+        lblSaludo.setBounds(10,20,300,30);
+        add(lblSaludo);
+    }
+
+    public static void main(String[] args) {
+        _p139_HolaMundoGUI app = new _p139_HolaMundoGUI();
+        app.setBounds(10, 10, 400, 300);
+        app.setLocationRelativeTo(null);
+        app.setVisible(true);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
